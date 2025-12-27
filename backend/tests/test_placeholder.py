108859150eff69c65683cb552aefe511b4aa8d3a
Sai Kiran (Mike) Jattu <sjattu@ualberta.ca>
2025-12-27 01:38:30 -0700

    added a test_placeholder

---
+++ backend/tests/test_placeholder.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
+
+
+from unittest import TestCase
+
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
