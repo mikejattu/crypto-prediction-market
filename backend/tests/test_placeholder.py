0a6ad059c9ae09975065c1e28126dab9ffa38b08
Sai Kiran (Mike) Jattu <sjattu@ualberta.ca>
2025-12-27 01:40:47 -0700

    changed the linter thing and also solved the test case

--- backend/tests/test_placeholder.py
+++ backend/tests/test_placeholder.py
@@ -4,17 +4,3 @@ from unittest import TestCase
 class TryTesting(TestCase):
     def test_always_passes(self):
         self.assertTrue(True)
-
-    def test_always_fails(self):
-        self.assertTrue(False)
-
-
-from unittest import TestCase
-
-
-class TryTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
-
-    def test_always_fails(self):
-        self.assertTrue(False)
